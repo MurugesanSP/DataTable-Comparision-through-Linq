f942dd71425f37864153d694942a998c401c791d
Murugesa Pandian <sharpcoder.p@hotmail.com>
2015-08-07 10:09:40 +0530

    DataTable to Linq
    Comparing the two C# - DataTable using the Linq method "Except" to find
    the unique records from both.

---
+++ WebApplication2/WebForm1.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>First Book List</h1>
+    <asp:GridView runat="server" ID="bookgrd"></asp:GridView>
+
+        <h1>Second Book List</h1>
+        <asp:GridView runat="server" ID="GridView1"></asp:GridView>
+        
+        <h1>Unique Book List</h1>
+        <asp:GridView runat="server" ID="GridView2"></asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataSet ds1 = new DataSet();
+            ds.ReadXml(@"D:\Books.xml");
+
+
+            bookgrd.DataSource = ds.Tables[0];
+            bookgrd.DataBind();
+
+            IEnumerable<DataRow> tbl1row = ds.Tables[0].AsEnumerable();
+            ds1.ReadXml(@"D:\Books2.xml");
+
+
+            
+            IEnumerable<DataRow> tbl2row = ds1.Tables[0].AsEnumerable();
+            GridView1.DataSource = ds1.Tables[0];
+            GridView1.DataBind();
+           
+            var items = tbl2row.AsEnumerable().Select(r => r.Field<string>("id"))
+        .Except(tbl1row.AsEnumerable().Select(r => r.Field<string>("id")));
+
+
+
+            DataTable TableC = (from row in tbl2row.AsEnumerable()
+                                join id in items
+                                on row.Field<string>("id") equals id
+                                select row).CopyToDataTable();
+
+            GridView2.DataSource = TableC;
+            GridView2.DataBind();
+
+            
+        }
+    }
+}
